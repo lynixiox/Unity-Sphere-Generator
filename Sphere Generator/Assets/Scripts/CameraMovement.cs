58a83ec7654e9ff3fdf0854197cc2cf72323c50c
Gandalf <lynxdev@protonmail.com>
2023-01-15 23:25:24 +0200

    Camera Script rotates around parent

--- Sphere Generator/Assets/Scripts/CameraMovement.cs
+++ Sphere Generator/Assets/Scripts/CameraMovement.cs
@@ -10,9 +10,9 @@ public class CameraMovement : MonoBehaviour
     public float zoomSensitivity = 20.0f;
     public float zoomSpeed = 20.0f;
     public float zoomMin = 5.0f;
-    publci float zoomMax = 80.0f;
+    public float zoomMax = 80.0f;
 
-    privat float zoom;
+    private float zoom;
 
     // Start is called before the first frame update
     void Start()
@@ -24,10 +24,25 @@ public class CameraMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Check if the mouse has been clicked and route the camera based on the mouse movement//
         if(Input.GetMouseButton(0))
         {
-            float h = 5 * Input.GetAxis("Mouse y");
+            float h = 5 * Input.GetAxis("Mouse Y");
             float y = (5 * Input.GetAxis("Mouse X")) * -1;
+
+            Vector3 axisOfRotaion = new Vector3(h,y, 0) * 10;
+            camera.transform.LookAt(parent.transform.position);
+            camera.transform.RotateAround(parent.transform.position, axisOfRotaion, 30 * Time.deltaTime);
         }
+
+        zoom -=  Input.GetAxis("Mouse ScrollWheel") * zoomSensitivity;
+        zoom = Mathf.Clamp(zoom, zoomMin, zoomMax);
+
+
+    }
+
+    void LateUpdate()
+    {
+        camera.fieldOfView = Mathf.Lerp(camera.fieldOfView, zoom, Time.deltaTime * zoomSpeed);
     }
 }
