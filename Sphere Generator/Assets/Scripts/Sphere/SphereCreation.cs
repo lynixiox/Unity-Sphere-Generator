890a7fb84242fc60b115fbd6508205554d786448
Gandalf <lynxdev@protonmail.com>
2023-01-17 16:56:19 +0200

    Sphere Creation

--- Sphere Generator/Assets/Scripts/Sphere/SphereCreation.cs
+++ Sphere Generator/Assets/Scripts/Sphere/SphereCreation.cs
@@ -25,14 +25,15 @@ public class SphereCreation : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        finalGrid = 
+        finalGrid = size_n_grid(size);
+        CreateObjects();
     }
 
     sHexGrid size_n_grid(int size)
     {
         if(size == 0)
         {
-            return CreateZeroSizeSphere();
+            return CreateZeroSphere();
         }
         else
         {
@@ -226,5 +227,144 @@ public class SphereCreation : MonoBehaviour
                 t.tiles[i] = grid.tiles[icosahedronTileNumbers[t.id, i]];
             }
         }
+
+        Debug.Log("Step 6");
+        for(int i = 0; i < 5; i++)
+        {
+            AddCorner(i, grid, 0, icosahedronTileNumbers[0, (i+4) % 5], icosahedronTileNumbers[0,i]);
+        }
+
+        Debug.Log("Step 7");
+        for(int i = 0; i < 5; i++)
+        {
+            AddCorner(i + 5, grid, 3, icosahedronTileNumbers[3, (i+4) % 5], icosahedronTileNumbers[3,i]);
+        }
+
+        Debug.Log("Step 8");
+        AddCorner(10, grid, 10, 1, 8);
+        AddCorner(11, grid, 1, 10, 6);
+        AddCorner(12, grid, 6, 10, 7);
+        AddCorner(13, grid, 6, 7, 11);
+        AddCorner(14, grid, 11, 7, 2);
+        AddCorner(15, grid, 11, 2, 9);
+        AddCorner(16, grid, 9, 2, 5);
+        AddCorner(17, grid, 9, 5, 4);
+        AddCorner(18, grid, 4, 5, 8);
+        AddCorner(19, grid, 4, 8, 1);
+
+        Debug.Log("Step 9");
+        foreach(sCorner c in grid.corners)
+        {
+            for(int i = 0 ; i < 3; i++)
+            {
+                c.corners[i] = c.tiles[i].corners[(sTileMaths.CalculatePosition(c.tiles[i],c) + 1) % 5];
+            }
+        }
+
+        Debug.Log("Step 10");
+        int newEdgeID = 0 ;
+        foreach(sTile t in grid.tiles)
+        {
+            for(int i = 0; i < 5; i++)
+            {
+                AddEdge(newEdgeID, grid, t.id, icosahedronTileNumbers[t.id, i]);
+                newEdgeID++;
+            }
+        }
+
+        Debug.Log("11");
+        return grid;
     }
+
+    sHexGrid CreateSubdividedSphere(sHexGrid previous)
+    {
+        sHexGrid nGrid = new sHexGrid();
+        nGrid.SetupHexGrid(previous.gridSize + 1);
+
+        int prev_tile_count = previous.tiles.Count;
+        int prev_corner_count = previous.corners.Count;
+
+        //old tiles//
+        for(int i = 0; i < prev_tile_count; i++)
+        {
+            nGrid.tiles[i].position = previous.tiles[i].position;
+            for(int k = 0; k < 3; k++)
+            {
+                nGrid.tiles[i + prev_tile_count].tiles[2 * k] = nGrid.tiles[previous.corners[i].corners[k].id + prev_tile_count];
+                nGrid.tiles[i + prev_tile_count].tiles[2 * k + 1] = nGrid.tiles[previous.corners[i].tiles[k].id];
+            }
+        }
+
+        //new corners//
+        int next_corner_id = 0;
+        foreach(sTile n in previous.tiles)
+        {
+            sTile t = nGrid.tiles[n.id];
+            for(int k = 0; k < t.edgeCount; k++)
+            {
+                AddCorner(next_corner_id, nGrid, t.id, t.tiles[(k + t.edgeCount -1) % t.edgeCount].id, t.tiles[k].id);
+                next_corner_id++;
+            }
+        }
+
+        //Connect the corners//
+        foreach(sCorner c in nGrid.corners)
+        {
+            for(int k = 0; k < 3; k++)
+            {
+                c.corners[k] = c.tiles[k].corners[(sTileMaths.CalculatePosition(c.tiles[k],c) + 1) % (c.tiles[k].edgeCount)];
+            }
+        }
+
+        //new edges//
+        int next_edge_id = 0;
+        foreach(sTile t in nGrid.tiles)
+        {
+            for(int k = 0 ; k < t.edgeCount; k++)
+            {
+                if(t.edges[k] == null)
+                {
+                    AddEdge(next_edge_id, nGrid, t.id, t.tiles[k].id);
+                    next_edge_id++;
+                }
+            }
+        }
+
+        return nGrid;
+    }
+
+
+    void AddCorner(int id, sHexGrid grid, int t1, int t2, int t3)
+    {
+        sCorner c = grid.corners[id];
+        sTile[] t = {grid.tiles[t1], grid.tiles[t2], grid.tiles[t3]};
+        Vector3 pos = t[0].position + t[1].position + t[2].position;
+        c.potision = sTileMaths.normal(pos);
+
+        for(int i = 0 ; i < 3; i++)
+        {
+            t[i].corners[sTileMaths.CalculatePosition(t[i], t[(i+2) % 3 ])] = c;
+            c.tiles[i] = t[i];
+        }
+    }
+
+    void AddEdge(int id, sHexGrid grid, int t1, int t2)
+    {
+        sEdge e = grid.edges[id];
+        sTile[] t = {grid.tiles[t1], grid.tiles[t2]};
+        sCorner[] c = 
+        {
+            grid.corners[t[0].corners[sTileMaths.CalculatePosition(t[0], t[1])].id],grid.corners[t[0].corners[(sTileMaths.CalculatePosition(t[0], t[1])+1)%t[0].edgeCount].id]
+        };
+
+        for(int i = 0 ; i < 2; i++)
+        {
+            t[i].edges[sTileMaths.CalculatePosition(t[i], t[(i+1) % 2])] = e;
+            e.tiles[i] = t[i];
+
+            c[i].edges[sTileMaths.CalculatePosition(c[i], c[(i + 1) % 2])] = e;
+            e.corners[i] = c[i];
+        }
+    }
+
 }
